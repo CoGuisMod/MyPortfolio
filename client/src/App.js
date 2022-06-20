1bb4b8cad91a9c95d4392333e7ce99e0113edcd8
CoGuisMod <coguismod@gmail.com>
2022-06-20 14:50:04 -0500

    Rick and morty wiki work addition

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Footer from "./components/Footer/Footer";
+import Work from "./components/Work/Work";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Work />
       <Contact />
       <Footer />
     </div>
